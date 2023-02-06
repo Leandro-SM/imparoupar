af11d2e01066c43e817b93a68ff3572fbc6f3dbd
Leandro S. Medeiros <65362520+Leandro-SM@users.noreply.github.com>
2023-02-06 12:33:22 -0300

    Add files via upload

---
+++ parouimpar.py
@@ -0,0 +1,10 @@
+print("Programinha para ver se é Par ou Impar \n")
+n = int(input("Digite um número: \n"))
+if  n % 2 == 0:
+    print('O número', n ,'é par! \n')
+else:
+    print('O número ', n ,' é impar! \n')
+print('\n')
+
+
+
